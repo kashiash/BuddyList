916d306cae4017f2db5a230a4565a6971e766230
Jacek Kosinski <kashiash@gmail.com>
2023-01-22 23:29:40 +0100

    Initial Commit

---
+++ BuddyList/BuddyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyListApp.swift
+//  BuddyList
+//
+//  Created by Jacek Kosiński G on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BuddyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
